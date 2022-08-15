72b90d077e69c4c605255472741019eddbad71d9
Ng Guoyou <gyng@users.noreply.github.com>
2022-08-16 06:44:47 +0800

    Use more reliable left arrow for mobile

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -12,7 +12,7 @@ export const Nav: React.FC<{ isEditing: boolean; editable: boolean }> = (
       <ul>
         <li>
           <Link href="/">
-            <a style={{ textDecoration: "none" }}>⯇ Album list</a>
+            <a style={{ textDecoration: "none" }}>← Album list</a>
           </Link>
         </li>
 
