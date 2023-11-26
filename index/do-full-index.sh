8ed3e26ecccd6be95da0ccbe87cc54c50d877cde
Ng Guoyou <gyng@users.noreply.github.com>
2023-11-27 03:36:24 +0800

    fix: add uppercase JPG to glob pattern for indexing in script

--- index/do-full-index.sh
+++ index/do-full-index.sh
@@ -2,5 +2,6 @@
 set -euox pipefail
 
 poetry run python index.py index --glob "../src/public/data/albums/**/*.jpg" --dbpath "search.sqlite" &&
+    poetry run python index.py index --glob "../src/public/data/albums/**/*.JPG" --dbpath "search.sqlite" &&
     poetry run python index.py search --query "burger" --dbpath "search.sqlite" &&
     cp search.sqlite ../src/public/search.sqlite
